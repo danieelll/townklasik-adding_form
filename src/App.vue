cf48133ec7f42fbc449a16ee1d8b04092e465987
Robert Lewis <rswl86@gmail.com>
2020-09-21 07:46:29 -0400

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,55 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="container">
+    <b-row class="mb-3">
+      <b-col md="3">
+        <b-form-input v-model="filter" type="search" id="filterInput" placeholder="Type to Search"></b-form-input>
+      </b-col>
+    </b-row>
+    <b-row>
+      <b-col>
+        <b-table striped hover outlined :items="posts" :fields="fields" :filter="filter">
+          <template v-slot:cell(actions)="data">
+            <b-button variant="danger" @click="deleteItem(data.item.id)">Delete</b-button>
+          </template>
+        </b-table>
+      </b-col>
+    </b-row>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      fields: ["userId", "id", "title", "actions"],
+      filter: "",
+      posts: [
+        {
+          userId: 1,
+          id: 1,
+          title:
+            "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+        },
+        {
+          userId: 1,
+          id: 2,
+          title: "qui est esse",
+        },
+        {
+          userId: 1,
+          id: 3,
+          title: "ea molestias quasi exercitationem repellat qui ipsa sit aut",
+        },
+      ],
+    };
+  },
+  methods: {
+    deleteItem(id) {
+      alert(`Sample delete of post with ID: ${id}`);
+    },
+  },
+};
 </script>
 
 <style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,17 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+//import css files
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+// Install BootstrapVue
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
